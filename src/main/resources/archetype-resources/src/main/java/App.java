c7fa8f9680d2c34f3fd289fcff6e5073c8851e50
Oto Junior <oto.junior@spread.com.br>
2013-10-30 16:41:11 -0200

    Ajustes em artefatos

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,15 @@
+package $package;
+
+/**
+ * @author 
+ *
+ */
+public class App {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("App:main");
+	}
+}
